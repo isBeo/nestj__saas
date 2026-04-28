6e70f4d61198b4e7bcd8561fb47876f360c63163
Beo Dammy <beodammy@gmail.com>
2026-04-28 22:39:59 +0100

    feat: setup prisma postgres docker workspace

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,4 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log('API is running...'))
+  .catch((err) => console.error('Error starting API:', err))
+  .finally(() => console.log('Bootstrap process completed.'));
